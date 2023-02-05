a01a41407ec989a0aa14072b6f4ce3ce17532655
wangenius <wangenius@qq.com>
2023-02-05 21:28:10 +0800

    Delete end directory

--- end/@constant/constant.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.handle = void 0;
-const electron_1 = require("electron");
-const handle = (channel, listener) => electron_1.ipcMain.handle(channel, listener);
-exports.handle = handle;
--- end/preload.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld("invoke", electron_1.ipcRenderer.invoke);
-electron_1.contextBridge.exposeInMainWorld("clipboard", electron_1.clipboard);
-electron_1.contextBridge.exposeInMainWorld("shell", electron_1.shell);
